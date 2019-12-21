793440ba0f4850e66daf1085f26e36fbd20b0440
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2019-12-21 18:55:21 +0000

    Remove footer

--- my-blog-starter/src/components/layout.js
+++ my-blog-starter/src/components/layout.js
@@ -63,9 +63,6 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     )
